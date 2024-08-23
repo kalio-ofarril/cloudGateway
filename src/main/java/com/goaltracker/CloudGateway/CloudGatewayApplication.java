cdcf239249e3201626ec6fcf5b40c84edd706968
Kalio <kalio.ofvil@gmail.com>
2024-08-23 11:10:31 -0600

    Changing cw config

--- src/main/java/com/goaltracker/CloudGateway/CloudGatewayApplication.java
+++ src/main/java/com/goaltracker/CloudGateway/CloudGatewayApplication.java
@@ -3,10 +3,19 @@ package com.goaltracker.CloudGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import reactor.netty.http.client.HttpClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CloudGatewayApplication {
+
+	@Bean
+	public HttpClient httpClient() {
+		return HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
